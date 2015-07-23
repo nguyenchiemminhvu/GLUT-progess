269d1af8eedc11b6e8ae10f9ddbf7fb68060f743
Minh Vu <nguyenchiemminhvu@gmail.com>
2015-07-23 23:19:44 +0700

    edited

--- Textures/main.cpp
+++ Textures/main.cpp
@@ -71,7 +71,7 @@ void drawScene() {
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientLight);
 
 	GLfloat directedLight[] = {0.7f, 0.7f, 0.7f, 1.0f};
-	GLfloat directedLightPos[] = {-10.0f, 15.0f, 20.0f, 0.0f};
+	GLfloat directedLightPos[] = { 10.0f, 15.0f, 20.0f, 0.0f};
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, directedLight);
 	glLightfv(GL_LIGHT0, GL_POSITION, directedLightPos);
 
@@ -98,30 +98,39 @@ void drawScene() {
 	glEnd();
 
 	//Back
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glColor3f(1.0f, 1.0f, 1.0f);
 	glBegin(GL_TRIANGLES);
 
 	glNormal3f(0.0f, 0.0f, 1.0f);
 	glTexCoord2f(0.0f, 0.0f);
 	glVertex3f(-2.5f, -2.5f, -2.5f);
-	glTexCoord2f(5.0f, 5.0f);
+	glTexCoord2f(1.0f, 1.0f);
 	glVertex3f(0.0f, 2.5f, -2.5f);
-	glTexCoord2f(10.0f, 0.0f);
+	glTexCoord2f(2.0f, 0.0f);
 	glVertex3f(2.5f, -2.5f, -2.5f);
 
 	glEnd();
 
 	//Left
 	glDisable(GL_TEXTURE_2D);
-	glColor3f(1.0f, 0.7f, 0.3f);
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _textureId);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glColor3f(1.0f, 1.0f, 1.0f);
 	glBegin(GL_QUADS);
 
 	glNormal3f(1.0f, 0.0f, 0.0f);
+	glTexCoord2d(0.0f, 0.0f);
 	glVertex3f(-2.5f, -2.5f, 2.5f);
+	glTexCoord2d(1.0f,0.0f);
 	glVertex3f(-2.5f, -2.5f, -2.5f);
+	glTexCoord2d(1.0f, 1.0f);
 	glVertex3f(-2.5f, 2.5f, -2.5f);
+	glTexCoord2d(0.0f, 1.0f);
 	glVertex3f(-2.5f, 2.5f, 2.5f);
 
 	glEnd();
