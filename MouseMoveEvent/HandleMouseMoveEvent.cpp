fd2d2c3d6e0a606288c4bb1ac9b93e180b7520fb
Minh Vu <nguyenchiemminhvu@gmail.com>
2015-07-26 16:39:54 +0700

    update rotate camera

--- MouseMoveEvent/HandleMouseMoveEvent.cpp
+++ MouseMoveEvent/HandleMouseMoveEvent.cpp
@@ -4,6 +4,7 @@
 
 int currentX, currentY;
 float Hangle = 0, Vangle = 0;
+float camera_h_angle = 0, camera_v_angle = 0;
 
 void handleResize(int w, int h) {
 
@@ -20,8 +21,12 @@ void drawing()  {
     glClearColor(0.2, 1.0, 0.6, 1.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    glRotatef(camera_h_angle, 0, 1, 0);
+    glRotatef(camera_v_angle, 1, 0, 0);
 
     glTranslatef(0, 0, -10);
+
+    glPushMatrix();
     glRotatef(Hangle, 0, 1, 0);
     glRotatef(Vangle, 1, 0, 0);
 
@@ -34,6 +39,7 @@ void drawing()  {
     glVertex3f(0, 1, 0);
 
     glEnd();
+    glPopMatrix();
 
     glutSwapBuffers();
 }
@@ -75,7 +81,40 @@ void handleMouseMove(int x, int y)  {
     currentY = y;
 
     update(0);
-    //glutWarpPointer(currentX, currentY);
+}
+
+void handleMousePassiveMove(int x, int y)   {
+
+    if(x > currentX)    {
+
+        camera_h_angle += 0.2f;
+        if(camera_h_angle > 360)
+            camera_h_angle -= 360;
+    }
+    else if(x < currentX)   {
+
+        camera_h_angle -= 0.2f;
+        if(camera_h_angle < 0)
+            camera_h_angle += 360;
+    }
+
+    if(y > currentY)   {
+
+        camera_v_angle += 0.2f;
+        if(camera_v_angle > 360)
+            camera_v_angle -= 360;
+    }
+    else if(y < currentY)   {
+
+        camera_v_angle -= 0.2f;
+        if(camera_v_angle < 0)
+            camera_v_angle += 360;
+    }
+
+    currentX = x;
+    currentY = y;
+
+    update(0);
 }
 
 void handleKeyPress(unsigned char key, int x, int y)    {
@@ -88,16 +127,18 @@ int main(int argc, char** argv)    {
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(400,400);
+    glutInitWindowSize(glutGet(GLUT_SCREEN_WIDTH), glutGet(GLUT_SCREEN_HEIGHT));
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_COLOR_MATERIAL);
 
     glutCreateWindow("Test");
-    currentX = currentY = 200;
+    currentX = glutGet(GLUT_SCREEN_WIDTH) / 2;
+    currentY = glutGet(GLUT_SCREEN_HEIGHT) / 2;
     glutWarpPointer(currentX,currentY);
 
     glutDisplayFunc(drawing);
     glutMotionFunc(handleMouseMove);
+    glutPassiveMotionFunc(handleMousePassiveMove);
     glutKeyboardFunc(handleKeyPress);
     glutReshapeFunc(handleResize);
 
